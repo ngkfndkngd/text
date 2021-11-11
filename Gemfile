93d3cbdeb45669cea20fb61febd57e733b0f9d14
ngkfndkngd <huku.0033@gmail.com>
2021-11-11 13:05:24 +0000

    [Add] cocoon導入

--- Gemfile
+++ Gemfile
@@ -72,6 +72,7 @@ gem 'bootstrap', '~> 4.5'
 gem 'jquery-rails'
 gem 'font-awesome-sass', '~> 5.13'
 gem "enum_help"
+gem 'cocoon'
 
 
 gem 'dotenv-rails'
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,14 @@
 class Recipe < ApplicationRecord
   
+  # レシピ投稿画像 refile定義
   attachment :recipe_image
+  # アソシエーション（親）
   belongs_to :user
+  # アソシエーション（子）
   has_many :materials, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :reviews, dependent: :destroy
+  # 複数のモデルを登録・更新させる
+  accepts_nested_attributes_for :materials, allow_destroy: true
+  
 end
