9fb64f57500991bdc72dfb4199c06fbd98151a88
ngkfndkngd <huku.0033@gmail.com>
2021-11-18 11:44:53 +0000

    [Add] RSpec

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,9 @@ class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  # バリデーション（名前：空を防ぐ、最低2文字、最高20字）
-  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
+  # バリデーション（名前：空を防ぐ、一意性があること、最低2文字、最高20字）
+  validates :name, presence: true, uniqueness: true,
+                   length: { minimum: 2, maximum: 20 }
   # （自己紹介：最高50字まで）
   validates :introduction, length: { maximum: 50 }
 end
