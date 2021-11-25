13c12a563d7c814cbf2c4c19cd8d56e658ab2bf5
ngkfndkngd <huku.0033@gmail.com>
2021-11-25 10:41:09 +0000

    [add]カラム

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_053257) do
+ActiveRecord::Schema.define(version: 2021_11_25_103529) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
