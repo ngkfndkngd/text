49e8508fbf508072764d29b15ea4cce099b25c1b
ngkfndkngd <huku.0033@gmail.com>
2021-11-25 12:37:14 +0000

    [修正]top

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,6 @@ class Tag < ApplicationRecord
   has_many :recipe_tags, dependent: :destroy
   has_many :recipes, through: :recipe_tags
   #中間テーブルであるrecipe_tagモデルを介してのrecipeモデルとの関連付けを記述。
+
+  attachment :tag_image
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,31 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-    Tag.create!(
+Tag.create!(
   [
     {
       name: '発酵食品',
+      tag_image: File.open('./app/assets/images/tag1.jpeg')
     },
     {
       name: '海藻類',
+      tag_image: File.open('./app/assets/images/tag2.png')
     },
     {
       name: '穀物類',
+      tag_image: File.open('./app/assets/images/tag3.png')
     },
     {
       name: 'きのこ類',
+      tag_image: File.open('./app/assets/images/tag4.jpeg')
     },
     {
       name: '野菜類',
+      tag_image: File.open('./app/assets/images/tag5.jpeg')
     },
     {
       name: '簡単レシピ',
+      tag_image: File.open('./app/assets/images/tag6.png')
     }
   ]
 )
